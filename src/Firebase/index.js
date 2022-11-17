352c436f237d64e92e206d0db8e3e5f8fd0958b0
Tymon Tian <tymon.tian@student.tdsb.on.ca>
2022-11-17 13:51:50 -0500

    Firebase Setup

---
+++ src/Firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSaiEbxgKc5y7ZMkC6wDjxW7s4oCaon04",
+  authDomain: "ics4u-c5db2.firebaseapp.com",
+  projectId: "ics4u-c5db2",
+  storageBucket: "ics4u-c5db2.appspot.com",
+  messagingSenderId: "362635239545",
+  appId: "1:362635239545:web:9c241d0e99eca3466934a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
